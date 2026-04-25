c74c83c930d2e564c6c7d1cf47287dca34ac2412
Narendra <dnarendra4510@gmail.com>
2026-04-25 18:34:48 +0530

    Updated Two Sum Code

---
+++ src/main/java/com/naruto/dsa/Linear/arrays/_001_TwoSum.java
@@ -0,0 +1,32 @@
+package com.naruto.dsa.Linear.arrays;
+
+import java.util.Arrays;
+
+public class _001_TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        _001_TwoSum obj = new _001_TwoSum();
+
+        int[] nums = {2,7,11,15};
+        int target = 18;
+
+        int[] result = obj.twoSum(nums, target);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
