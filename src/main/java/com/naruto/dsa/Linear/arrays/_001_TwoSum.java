3dc407e5b0f0586c556382c5a875562027525838
Narendra <dnarendra4510@gmail.com>
2026-05-03 19:52:59 +0530

    Hashmap Approach Code Added

--- src/main/java/com/naruto/dsa/Linear/arrays/_001_TwoSum.java
+++ src/main/java/com/naruto/dsa/Linear/arrays/_001_TwoSum.java
@@ -1,5 +1,4 @@
 package com.naruto.dsa.Linear.arrays;
-
 import java.util.Arrays;
 
 public class _001_TwoSum {
@@ -14,19 +13,37 @@ public class _001_TwoSum {
                 }
             }
         }
-
         return new int[]{};
     }
-
-    public static void main(String[] args) {
+    static void main(String[] args) {
 
         _001_TwoSum obj = new _001_TwoSum();
-
         int[] nums = {2,7,11,15};
         int target = 18;
-
         int[] result = obj.twoSum(nums, target);
-
         System.out.println(Arrays.toString(result));
     }
 }
+
+
+//Approach2:- HashMap
+// Leetcode code so here main class not use.
+
+//import java.util.HashMap;
+//
+//public class Solution {
+//    public int[] twoSum(int[] nums, int target) {
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            int complement = target - nums[i];
+//
+//            if (map.containsKey(complement)) {
+//                return new int[]{map.get(complement), i};
+//            }
+//
+//            map.put(nums[i], i);
+//        }
+//        return new int[]{};
+//    }
+//}
