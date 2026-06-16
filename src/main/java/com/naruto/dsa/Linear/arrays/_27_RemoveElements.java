d7eaac4070e75dcea54097f4aa0456caec9a710a
Narendra <dnarendra4510@gmail.com>
2026-06-16 12:27:36 +0530

    3 Array Problems Solved

--- src/main/java/com/naruto/dsa/Linear/arrays/_27_RemoveElements.java
+++ src/main/java/com/naruto/dsa/Linear/arrays/_27_RemoveElements.java
@@ -1,4 +1,34 @@
 package com.naruto.dsa.Linear.arrays;
 
 public class _27_RemoveElements {
+
+
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        _27_RemoveElements obj = new _27_RemoveElements();
+
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        int k = obj.removeElement(nums, val);
+
+        System.out.println("k = " + k);
+
+        System.out.print("Modified array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 }
